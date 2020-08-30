1b787a493906271d2c6c116c76c105aade7d48f1
mhafet17 <do.crazy192@gmail.com>
2020-08-30 22:59:03 +0700

    update batch 5

--- Batch 5 (Class)/Latihan1.js
+++ Batch 5 (Class)/Latihan1.js
@@ -1,15 +1,42 @@
-class Car {
-    constructor (manufacture,color) {
-        this.manufacture = manufacture;
-        this.color = color;
-        this.engineActive = false;
+class Kucing {
+    constructor(jenis, warna, makanan) {
+        this.jenis = jenis,
+            this.warna = warna,
+            this.makanan = makanan
     }
 }
 
-const johnCar = new Car("Honda", "Red");
-const adamCar = new Car("Tesla", "Black");
- 
-adamCar.color = 'White'
+// instansiasi
+const type1 = new Kucing("anggora", "kuning", "popan");
+const type2 = new Kucing("garfield", "coklat", "popan");
 
-console.log(johnCar.manufacture);
-console.log(adamCar.color);
+console.log(type1.jenis);
+console.log(type2.warna);
+
+class Car {
+
+    // Sama seperti function constructor
+    constructor(manufacture, color) {
+        this.manufacture = manufacture;
+        this.color = color;
+        this.enginesActive = false;
+    }
+
+    // Sama seperti Car.prototype.startEngine
+    startEngines () {
+        console.log('Mobil dinyalakan...');
+        this.enginesActive = true;
+    }
+
+    // Sama seperti car.prototype.info
+    info () {
+        console.log(`Manufacture: ${this.manufacture}`);
+        console.log(`Color:  ${this.color}`);
+        console.log(`Engines: ${this.enginesActive ? "Active" : "Inactive"}`);
+    }
+
+}
+
+const johnCar = new Car("Honda", "Red");
+johnCar.startEngines();
+johnCar.info();
