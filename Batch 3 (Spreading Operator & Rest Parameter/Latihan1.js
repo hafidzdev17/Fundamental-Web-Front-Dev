a5200b1e865114da73f58e5121c6b705dc8f3bae
mhafet17 <do.crazy192@gmail.com>
2020-08-30 22:58:45 +0700

    update batch 3

--- Batch 3 (Spreading Operator & Rest Parameter/Latihan1.js
+++ Batch 3 (Spreading Operator & Rest Parameter/Latihan1.js
@@ -1,21 +1,22 @@
 // Spreading operator dituliskan dengan three consecutive dots (....). Sesuai namanya “Spread”, fitur baru ES6 ini digunakan untuk membentangkan nilai array atau lebih tepatnya iterable object menjadi beberapa elements. 
 
-const favorites = ["Seafood", "Salad", "Nugget", "Soup"];
- 
-console.log(...favorites);
+const seafod = ["Cumi-cumi", "Lobster", "Kepiting"];
+
+console.log(...seafod);
 
 /* Math.max() -> Mencari nilai terbesar */
 const numbers = [12, 32, 90, 12, 32];
- 
+
 // Sama seperti kita menuliskan
 // console.log(Math.max(numbers[0], numbers[1], numbers[2], numbers[3])
- 
-console.log(Math.max(...numbers));
+console.log(numbers);
+console.log('Terbesar : ' + Math.max(...numbers));
+console.log('Terkecil : ' + Math.min(...numbers));
 
 // Menggabungkan dua buah array
 const favorites = ["Seafood", "Salad", "Nugget", "Soup"];
 const others = ["Cake", "Pie", "Donut"];
- 
+
 const allFavorites = [...favorites, ...others]
- 
+
 console.log(allFavorites);
--- Batch 3 (Spreading Operator & Rest Parameter/Latihan2.js
+++ Batch 3 (Spreading Operator & Rest Parameter/Latihan2.js
@@ -1,4 +1,7 @@
-function sum(...numbers) {
+// Rest Parameter
+// Rest parameter dituliskan menggunakan three consecutive dots (...). Dengan rest parameter, kita dapat menggabungkan beberapa elemen menjadi satu array. Tentu teknik ini sangat bermanfaat ketika kita hendak membuat sebuah variadic function.
+
+function sum (...numbers) {
     var result = 0;
     for (let number of numbers) {
         result += number
@@ -6,4 +9,12 @@ function sum(...numbers) {
     return result
 }
 
-console.log(sum(1,2,3,4,5));
+console.log(sum(1, 2, 3, 4, 5));
+
+// Rest Parameter Destructuring
+const buah = ['apel', 'jeruk', 'mangga', 'nanas', 'pepaya', 'anggur'];
+const [buah1, buah2, buah3, ...semua] = buah;
+console.log(buah1);
+console.log(buah2);
+console.log(buah3);
+console.log(semua);
