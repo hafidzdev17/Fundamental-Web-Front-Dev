56548f807174192682a4ba611ba97e7fe4cae891
mhafet17 <do.crazy192@gmail.com>
2020-08-30 22:58:37 +0700

    update batch 2

--- Batch 2 (Destructuring)/Latihan1.js
+++ Batch 2 (Destructuring)/Latihan1.js
@@ -1,18 +1,20 @@
+// Destructuring pada JavaScript merupakan sintaks yang dapat mengeluarkan nilai dari array atau properties dari sebuah object ke dalam satuan yang lebih kecil.
+
 // Destucturing Object
-const profile = {
-    firstName: "John",
-    lastName: "Doe",
-    age: 18
+const biodata = {
+    namaDepan: "Hafid",
+    namaBelakang: "Masruri",
+    umur: 21
 }
- 
-const {firstName: namaDepan, lastName: namaAkhir, age: umur} = profile;
- 
-console.log(namaDepan, namaAkhir, umur);
+
+const pecahkan = { satu: namaDepan, dua: namaBelakang, tiga: umur } = biodata;
+
+console.log(pecahkan);
 
 // Destructuring Array
 const favorites = ["Seafood", "Salad", "Nugget", "Soup"];
-const [firstFood, secondFood, thirdFood, fourthFood,herFood = "Salad"] = favorites;
- 
+const [firstFood, secondFood, thirdFood, fourthFood, herFood = "Salad"] = favorites;
+
 console.log(firstFood);
 console.log(secondFood);
 console.log(thirdFood);
@@ -22,14 +24,14 @@ console.log(herFood);
 
 // Destructuring Assignment
 let a = 1;
-let b= 2;
- 
+let b = 2;
+
 console.log("Sebelum swap");
 console.log("Nilai a: " + a);
 console.log("Nilai b: " + b);
 
 [a, b] = [b, a]
- 
+
 console.log("Setelah swap");
 console.log("Nilai a: " + a);
 console.log("Nilai b: " + b);
